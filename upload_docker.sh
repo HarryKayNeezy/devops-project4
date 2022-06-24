0395dba76288b1deb109a6fa62247be71d51a308
Harry Kay Ablor <harrykayablor@Harrys-MacBook-Air.local>
2022-06-24 15:04:17 +0000

    devops project 4

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=h4rryk4y/devops_microservice_ml_kubernetes
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag devops_microservice_ml_kubernetes $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
